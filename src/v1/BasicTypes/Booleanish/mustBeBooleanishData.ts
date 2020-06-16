b6d09e06106641102d514ac7bb84570d9af75b73
Stuart Herbert <stuart@stuartherbert.com>
2020-06-16 12:31:55 +0100

    New: added mustBeBooleanishData()

---
+++ src/v1/BasicTypes/Booleanish/mustBeBooleanishData.ts
@@ -0,0 +1,70 @@
+//
+// Copyright (c) 2020-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+import { DataGuarantee, DataGuaranteeOptions } from "../../Archetypes";
+import { THROW_THE_ERROR } from "../../ErrorHandling";
+import { mustBe } from "../../Operators";
+import { DEFAULT_DATA_PATH } from "../../SupportingTypes";
+import { BooleanishDataOptions } from "./BooleanishDataOptions";
+import { DEFAULT_BOOLEANISH_RULES } from "./defaults/DEFAULT_BOOLEANISH_RULES";
+import { validateBooleanishData } from "./validateBooleanishData";
+
+/**
+ * `mustBeBooleanishData()` is a {@link DataGuarantee}. Use it to ensure
+ * that the unknown `input` contains a value that can be converted into
+ * `true` or `false`.
+ *
+ * See {@link validateBooleanishData} for a list of supported types and
+ * supported values.
+ *
+ * @param input
+ * the value to inspect
+ * @param onError
+ * if validation fails, we'll pass the error to this OnError handler
+ * @returns
+ * - `true` if `input` can be converted into `true`
+ * - `false` if `input` can be converted into `false`
+ * - does not return if `input` cannot be converted
+ *
+ * @category BasicTypes
+ */
+export const mustBeBooleanishData: DataGuarantee = (
+    input: unknown,
+    {
+        onError = THROW_THE_ERROR,
+        path = DEFAULT_DATA_PATH,
+        booleanish = DEFAULT_BOOLEANISH_RULES,
+    }: Partial<DataGuaranteeOptions> & Partial<BooleanishDataOptions> = {}
+): boolean => mustBe(input, { onError })
+    .next((x) => validateBooleanishData(path, x, { booleanish }))
+    .value();
