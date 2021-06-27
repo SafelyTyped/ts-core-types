317f32bcf9c00b6da74b9ce9040d697f0ed5b14c
Stuart Herbert <stuart@stuartherbert.com>
2021-06-27 11:28:20 +0100

    New: added AnyTypeValidator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,11 @@ For each release, changes are grouped under these headings:
 
 The following changes have been completed, and will be included in the next tagged release.
 
+### New
+
+* Archetypes
+  - added `AnyTypeValidator` type
+
 ## v0.3.1
 
 Released Wednesday, 26th May 2021.
--- src/v1/Archetypes/FunctionTypes/TypeValidator/AnyTypeValidator.ts
+++ src/v1/Archetypes/FunctionTypes/TypeValidator/AnyTypeValidator.ts
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2020-present Ganbaro Digital Ltd
+// Copyright (c) 2021-present Ganbaro Digital Ltd
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -32,5 +32,12 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //
 
-export * from "./TypeValidator";
-export * from "./AnyTypeValidator";
+import { TypeValidator } from "./TypeValidator";
+
+/**
+ * `AnyTypeValidator` is a type alias. Use it wherever any {@link TypeValidator}
+ * is valid.
+ *
+ * @category Archetypes
+ */
+export type AnyTypeValidator = TypeValidator<any>;
